4ebb37ef7b551c55fd959958d5b77500de58686a
Oleksii Orel <oorel@redhat.com>
2026-01-27 00:34:32 +0200

    chore: add EPL-2.0 license headers to all files
    Add Eclipse Public License 2.0 (EPL-2.0) headers to all files:
    - README.md (HTML comment)
    - scripts/deploy-che-ipv6-chectl.sh (shell comment)
    - scripts/test-ipv6-validation.sh (shell comment)
    - scripts/deploy-che-ipv6-chectl.md (HTML comment)
    - scripts/test-ipv6-validation.md (HTML comment)
    
    Copyright (c) 2026 Red Hat, Inc.
    SPDX-License-Identifier: EPL-2.0
    
    Signed-off-by: Oleksii Orel <oorel@redhat.com>

--- scripts/test-ipv6-validation.sh
+++ scripts/test-ipv6-validation.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#
+# Copyright (c) 2026 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+#
+
 # IPv6 Validation Testing Script for Eclipse Che Dashboard
 #
 # This script automates testing of IPv6 support in Eclipse Che Dashboard PR-1442
